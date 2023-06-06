750cc1212003189401de8708206880a4c3fc215b
Caleb Bushard <bloodyybass@gmail.com>
2023-06-06 11:54:34 -0400

    proof of concept for fetch requests, and date/time/location variables

--- index.js
+++ index.js
@@ -1,21 +1,54 @@
-let currentWeatherNYC = "https://api.meteomatics.com/2023-06-06T10:05:00.000-04:00/t_2m:F/40.7127281,-74.0060152/json?model=mix"
-username='_stevens';
-password='NMU50sm4cs';
+//current dateTime variable
+let current = new Date();
+let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
+let cTime = current.getHours() + ":" + current.getMinutes() + ":" + current.getSeconds();
+let dateTime = cDate + 'T' + cTime + '+05:00';
+
+//postal codes
+let NYCpostalCode = 'postal_US10001'
+let TampapostalCode = 'postal_US33592'
+let SFpostalCode = 'postal_US94016'
+let SeattlepostalCode = 'postal_US98101'
+let ChicagopostalCode = 'postal_US60007'
+
+//working fetch requests
+username='_stevens'
+password='NMU50sm4cs'
 let headers = new Headers();
 headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
 
-// function fetchData() {
-//     const username = _stevens;
-//     const password = NMU50sm4cs;
-//     let currentWeatherNYC = "https://api.meteomatics.com/2023-06-06T10:05:00.000-04:00/t_2m:F/40.7127281,-74.0060152/json?model=mix";
-
-//     fetch(currentWeatherNYC)
-//           .then(response => response.json())
-//           .then(data => {}
-// }
-
-fetch(currentWeatherNYC)
-    .then(response => response.text())
-    .then(data => {
-    console.log(data);
-    });
+fetch('https://login.meteomatics.com/api/v1/token', {
+    method: 'GET', headers: headers
+}).then(function (resp) {
+    return resp.json();
+}).then(function (data) {
+    var token = data.access_token;
+    //console.log(token);
+    fetch(`https://api.meteomatics.com/${dateTime}/t_2m:F/${NYCpostalCode}/json?access_token=${token}`) 
+        .then(function(r){
+            return r.text()
+        })
+        .then(console.log)
+    fetch(`https://api.meteomatics.com/${dateTime}/t_2m:F/${TampapostalCode}/json?access_token=${token}`) 
+        .then(function(r){
+            return r.text()
+        })
+        .then(console.log)
+    fetch(`https://api.meteomatics.com/${dateTime}/t_2m:F/${SFpostalCode}/json?access_token=${token}`) 
+        .then(function(r){
+            return r.text()
+        })
+        .then(console.log)
+    fetch(`https://api.meteomatics.com/${dateTime}/t_2m:F/${SeattlepostalCode}/json?access_token=${token}`) 
+        .then(function(r){
+            return r.text()
+        })
+        .then(console.log)
+    fetch(`https://api.meteomatics.com/${dateTime}/t_2m:F/${ChicagopostalCode}/json?access_token=${token}`) 
+        .then(function(r){
+            return r.text()
+        })
+        .then(console.log)
+}).catch(function (err) {
+    console.log('something went wrong', err);
+});
