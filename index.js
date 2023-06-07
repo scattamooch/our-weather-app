cb327567580c837279db33e6cb045bbfbb51e1fb
scattamooch <seanstevens729@gmail.com>
2023-06-07 13:07:37 -0400

    placeholder save

--- index.js
+++ index.js
@@ -21,8 +21,6 @@ function blankCities() {
     const rain = document.getElementById("precip_in");
     const humidity = document.getElementById("humidity");
     const wind = document.getElementById("wind_mph");
-    const dropdownValue = document.getElementById("citySelect").value;
-    const city = cities.find(city => city.name === dropdownValue); //find a city name in the cities data that matches the dropdown value
 
     conditionImg.src = uglyImgLink;
     cityName.textContent = "No city selected";
@@ -52,20 +50,21 @@ function renderCityNavBar(city, navBar) {
     navItem.textContent = city.name;
     navItem.value = city.name;
     navItem.id = city.name;
+    navItem.addEventListener("click", renderMainCity);
     navBar.appendChild(navItem);
 }
 
 function eventListeners() {
-    const navBar = document.getElementById("flexbox-container");
-    navBar.addEventListener("click", renderMainCity);
-
-    const modeBtn = document.getElementById("mode-toggle");
     window.addEventListener("keydown", (event) => {
         if (event.key === "l" || event.key === "L") {
             document.body.classList.toggle("dark-mode");
             document.body.classList.toggle("light-mode");
         }
     });
+
+    const imgElement = document.getElementById("main-image-container");
+    imgElement.addEventListener("mouseover", genericWeatherAdvisement);
+    imgElement.addEventListener("mouseout", hideWeatherAdvisement);
 }
 
 function renderMainCity() {
@@ -77,8 +76,8 @@ function renderMainCity() {
     const rain = document.getElementById("precip_in");
     const humidity = document.getElementById("humidity");
     const wind = document.getElementById("wind_mph");
-    const dropdownValue = document.getElementById("citySelect").value;
-    const city = cities.find(city => city.name === dropdownValue); //find a city name in the cities data that matches the dropdown value
+    const navBarValue = this.textContent;         //this.textContent assigns the value of the "click" to navBarValue so .find still works. Workaround for transitioning from dropdown to navbar
+    const city = cities.find(city => city.name === navBarValue); //find a city name in the cities data that matches the dropdown value
 
     conditionImg.src = city.img_url;
     cityName.textContent = city.name;
@@ -89,4 +88,32 @@ function renderMainCity() {
     humidity.textContent = city.Humidity + " %";
     wind.textContent = city.wind_speed + " mph";
 }
+
+function genericWeatherAdvisement(e) {
+    const mouseoverCaption = document.getElementById("random-div");
+    const x = e.clientX;
+    const y = e.clientY;
+
+    mouseoverCaption.style.left = x + "px";
+    mouseoverCaption.style.top = y + "px";
+
+    console.log("aslkdfjasflk")
+    const imgElement = document.getElementById("main-image-container");
+    const sunnyImg = "https://t3.ftcdn.net/jpg/02/50/08/68/360_F_250086872_srlXRqANYR2IbNfIylRDc3eMO9MinjnV.jpg"
+    const cloudyImg = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQp2EMAtfJuOYFCc_-iB4I2ITrd08fw6TzZjw&usqp=CAU"
+    const overcastImg = "https://freesvg.org/img/sivvus_weather_symbols_3.png"
+    const rainyImg = "https://s3.amazonaws.com/pix.iemoji.com/images/emoji/apple/ios-12/256/cloud-with-lightning-and-rain.png"
+    if (imgElement.src === sunnyImg) {
+        const sunnyCaption = document.createElement("div");
+        sunnyCaption.setAttribute("class", "sunny-caption");
+        sunnyCaption.textContent = "It's beautiful";
+        mouseoverCaption.appendChild(sunnyCaption);
+    }
+}
+function hideWeatherAdvisement() {
+    const mouseoverCaptionParent = document.getElementById("random-div");
+    const mouseoverCaption = document.querySelectorAll(".sunny-caption");
+    mouseoverCaptionParent.removeChild(mouseoverCaption);
+}
+
 init();
