be8b9c2e26171579a76f1d17e7d6761cdfe90914
scattamooch <71458730+scattamooch@users.noreply.github.com>
2023-06-07 10:27:15 -0400

    Update index.js
    Added some functions n stuff

--- index.js
+++ index.js
@@ -1,36 +1,92 @@
-//current dateTime variable
-let current = new Date();
-let cDate = current.getFullYear() + '-' + (current.getMonth() + 1) + '-' + current.getDate();
-let seconds
-let secondsUnfixed = current.getSeconds();
-let cTime = current.getHours().toString().padStart(2, '0') + ":" + current.getMinutes().toString().padStart(2, '0') + ":" + current.getSeconds().toString().padStart(2, '0');
-let dateTime = cDate + 'T' + cTime + '+05:00';
+//fetch the data
+//get the city names into the dropdown
+//selecting a city makes it appear in the li elements
 
-//postal codes
-let NYCpostalCode = 'postal_US10001'
-let TampapostalCode = 'postal_US33592'
-let SFpostalCode = 'postal_US94016'
-let SeattlepostalCode = 'postal_US98101'
-let ChicagopostalCode = 'postal_US60007'
+function init() {
+    blankCities();
+    getCities();
+    eventListeners();
+}
 
-//working fetch requests
-username='_stevens'
-password='NMU50sm4cs'
-let headers = new Headers();
-headers.set('Authorization', 'Basic ' + btoa(username + ":" + password));
+const Cities_URL = "http://localhost:3000/cities";
+let cities = [];
+const uglyImgLink = "https://cdn.dribbble.com/users/1798374/screenshots/10355316/media/75206ff46afe3dee7f2cd9a22426581f.png?compress=1&resize=400x300"
 
-fetch('https://login.meteomatics.com/api/v1/token', {
-    method: 'GET', headers: headers
-}).then(function (resp) {
-    return resp.json();
-}).then(function (data) {
-    var token = data.access_token;
-    //console.log(token);
-    fetch(`https://api.meteomatics.com/${dateTime}/t_2m:F,t_2m:C,precip_1h:mm,wind_speed_2m:mph,relative_humidity_2m:p/${NYCpostalCode}+${TampapostalCode}+${SFpostalCode}+${SeattlepostalCode}+${ChicagopostalCode}/json?access_token=${token}`) 
-        .then(function(r){
-            return r.json()
-        })
-        .then(console.log)
-}).catch(function (err) {
-    console.log('something went wrong', err);
-});
+function blankCities() {
+    const conditionImg = document.getElementById("main-image-container");
+    const cityName = document.getElementById("span-city-name");
+    const condition = document.getElementById("span-condition");
+    const temp = document.getElementById("temp_f");
+    const feelsLike = document.getElementById("feelslike_f");
+    const rain = document.getElementById("precip_in");
+    const humidity = document.getElementById("humidity");
+    const wind = document.getElementById("wind_mph");
+    const dropdownValue = document.getElementById("citySelect").value;
+    const city = cities.find(city => city.name === dropdownValue); //find a city name in the cities data that matches the dropdown value
+
+    conditionImg.src = uglyImgLink;
+    cityName.textContent = "No city selected";
+    condition.textContent = "No condition information available";
+    temp.textContent = "No temperature data available";
+    feelsLike.textContent = "No temperature data available";
+    rain.textContent = "No rainfall data available";
+    humidity.textContent = "No humidity data available";
+    wind.textContent = "No wind data available";
+}
+
+function getCities() {
+    fetch(Cities_URL)
+    .then(response => response.json())
+    .then(cityData => {
+        cities = cityData;
+        renderCities(cityData)
+    });
+}
+function renderCities(cities) {
+    const navBar = document.getElementById("flexbox-container");
+    cities.forEach(city => renderCityNavBar(city, navBar));
+}
+
+function renderCityNavBar(city, navBar) {
+    const navItem = document.createElement("div");
+    navItem.textContent = city.name;
+    navItem.value = city.name;
+    navItem.id = city.name;
+    navBar.appendChild(navItem);
+}
+
+function eventListeners() {
+    const navBar = document.getElementById("flexbox-container");
+    navBar.addEventListener("click", renderMainCity);
+
+    const modeBtn = document.getElementById("mode-toggle");
+    window.addEventListener("keydown", (event) => {
+        if (event.key === "l" || event.key === "L") {
+            document.body.classList.toggle("dark-mode");
+            document.body.classList.toggle("light-mode");
+        }
+    });
+}
+
+function renderMainCity() {
+    const conditionImg = document.getElementById("main-image-container");
+    const cityName = document.getElementById("span-city-name");
+    const condition = document.getElementById("span-condition");
+    const temp = document.getElementById("temp_f");
+    const feelsLike = document.getElementById("feelslike_f");
+    const rain = document.getElementById("precip_in");
+    const humidity = document.getElementById("humidity");
+    const wind = document.getElementById("wind_mph");
+    const dropdownValue = document.getElementById("citySelect").value;
+    const city = cities.find(city => city.name === dropdownValue); //find a city name in the cities data that matches the dropdown value
+
+    conditionImg.src = city.img_url;
+    cityName.textContent = city.name;
+    condition.textContent = city.condition;
+    temp.textContent = city.temperature + " °F";
+    feelsLike.textContent = city.feels_like + " °F";
+    rain.textContent = city.rain + " in";
+    humidity.textContent = city.Humidity + " %";
+    wind.textContent = city.wind_speed + " mph";
+}
+init();
